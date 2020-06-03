4c39f173dc2297bb55a4d357e8968336224a4cff
Sidner <sidner.magneli@gmail.com>
2020-06-03 12:02:16 +0200

    Added semicolons

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { useGoogleMaps } from 'react-hook-google-maps'
+import React from 'react';
+import { useGoogleMaps } from 'react-hook-google-maps';
 
-const API_KEY = 'AIzaSyCQrdVemCc5-Q6dP0kwKHHwCsH5XcQfXa8'
+const API_KEY = 'AIzaSyCQrdVemCc5-Q6dP0kwKHHwCsH5XcQfXa8';
 
 const  mapContainerStyle = {
    height: '60vh',
    width: '60vw',
    margin: '1em 0',
    borderRadius: '0.3em'
-}
+};
 
 const mapInitConf = {
    zoom: 7,
@@ -17,12 +17,12 @@ const mapInitConf = {
  };
 
 export default function Map(props) {
-   const { ref, map, google } = useGoogleMaps(API_KEY, mapInitConf)
+   const { ref, map, google } = useGoogleMaps(API_KEY, mapInitConf);
 
-   console.log(map)
+   console.log(map);
    console.log(google);
 
    return (
       <div style={mapContainerStyle} ref={ref}/>
-   )
+   );
 }
